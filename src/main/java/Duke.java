273433a31af811c5238edda6daf1ba09e944a132
gordon <hyeungac@gmail.com>
2025-08-25 20:20:42 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int task_count = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -28,18 +28,41 @@ public class Duke {
             else if(line.equalsIgnoreCase("list")){
                 int index = 0;
                 System.out.println("____________________________________________________________");
-                for (String task : list) {
+                for (Task task : list) {
                     if (index == task_count) {
                         break;
                     }
-
-                    System.out.println(index + ": " + task);
+                    int num = index + 1;
+                    System.out.print(num + ". " );
+                    task.printinfo();
                     index++;
                 }
                 System.out.println("____________________________________________________________");
             }
+            else if(line.contains("unmark")){
+                String index = line.substring(line.indexOf(" ")+1);
+                int ind =  Integer.parseInt(index);
+                ind--;
+                list[ind].setDone(false);
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:");
+                list[ind].printinfo();
+                System.out.println("____________________________________________________________");
+
+            }
+            else if(line.contains("mark")){
+                String index = line.substring(line.indexOf(" ")+1);
+                int ind =  Integer.parseInt(index);
+                ind--;
+                list[ind].setDone(true);
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done:");
+                list[ind].printinfo();
+                System.out.println("____________________________________________________________");
+
+            }
             else{
-                list[task_count++] = line;
+                list[task_count++] = new Task(line);
                 System.out.println("____________________________________________________________\n" +
                         "added: "+line + "\n"+
                         "____________________________________________________________\n");
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String name;
+    private boolean done;
+    public Task(String name) {
+        this.name = name;
+        done = false;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public boolean isDone() {
+        return done;
+    }
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+    public void printinfo(){
+        System.out.print("[" );
+        if (this.done){
+            System.out.print("X");
+        }
+        else{
+            System.out.print(" ");
+        }
+        System.out.println("] " + this.name);
+    }
+}
